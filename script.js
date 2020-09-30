4de5a99d29c7cee90b5a8370fd321ec01ce9b087
coderGeek14 <72160788+coderGeek14@users.noreply.github.com>
2020-09-30 23:54:02 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const ul = document.querySelector(".list-group");
+const button = document.querySelector(".sort-btn");
+var buttonBoolean = false;
 
+const courses = [
+	{
+		name: "JavaScript Course",
+		price: "500",
+	},
+	{
+		name: "MERN Course",
+		price: "2500",
+	},
+	{
+		name: "Django Course",
+		price: "800",
+	},
+	{
+		name: "Flutter Course",
+		price: "1000",
+	},
+	{
+		name: "Java Course",
+		price: "1200",
+	},
+];
